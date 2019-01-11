ff039e1a837903bcc75ccd806ce83414d7a514fc
lnguyen24794 <lamnt@fasttrack.edu.vn>
2019-01-11 15:18:38 +0700

    prims editor

---
+++ client/src/components/editor/ckeditor.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <ckeditor :editor="editor" v-model="editorData"></ckeditor>
+        <button v-on:click="emptyEditor()">Empty the editor</button>
+
+        <h2>Editor data</h2>
+        <div v-html="editorData"></div>
+    </div>
+</template>
+
+<script>
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>'
+            };
+        },
+        methods: {
+            emptyEditor() {
+                this.editorData = '';
+            }
+        }
+    }
+</script>
+
+
--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,9 @@ import App from "./App.vue";
 import router from "./routes";
 import store from "./store";
 
+import CKEditor from '@ckeditor/ckeditor5-vue';
+Vue.use( CKEditor );
+
 Vue.config.productionTip = false;
 
 new Vue({
