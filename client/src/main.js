cb3241ac804b3c6605618878a1d1f097f10a3236
lnguyen24794 <lnguyen24794@gmail.com>
2019-01-12 00:57:21 +0700

    ckeditor

--- client/src/main.js
+++ client/src/main.js
@@ -7,8 +7,14 @@ import CKEditor from '@ckeditor/ckeditor5-vue';
 import 'highlight.js/styles/default.css';
 import Highlight from 'vue-markdown-highlight';
 
+<<<<<<< HEAD
 Vue.use(Highlight)
 Vue.use( CKEditor );
+=======
+import CKEditor from '@ckeditor/ckeditor5-vue';
+Vue.use( CKEditor );
+
+>>>>>>> parent of 319d32b... Revert "prims editor"
 Vue.config.productionTip = false;
 
 new Vue({
