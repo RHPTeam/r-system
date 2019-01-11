4897e47b87c336df346e9c116d65c9c35d680b2b
lnguyen24794 <lnguyen24794@gmail.com>
2019-01-12 00:49:38 +0700

    ckeditor

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,12 @@ import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./routes";
 import store from "./store";
+import CKEditor from '@ckeditor/ckeditor5-vue';
+import 'highlight.js/styles/default.css';
+import Highlight from 'vue-markdown-highlight';
 
+Vue.use(Highlight)
+Vue.use( CKEditor );
 Vue.config.productionTip = false;
 
 new Vue({
--- client/src/views/lega/index.vue
+++ client/src/views/lega/index.vue
@@ -1,135 +1,28 @@
 <template>
-    <div class="ct_fl" >
-        <app-top-bar/>
-        <div class="r wrapper">
-            <div class="c_lg_3 c_md_4 c_xs_12 c_sm_12">
-                <app-site-menu :arNav="arNav"/>
-            </div>
-            <div class="c_lg_9 c_md_8 c_xs_12 c_sm_12">
-                <router-view />
-            </div>
-           
-        </div>
+    <div id="app">
+        <ckeditor :editor="editor" v-model="editorData"></ckeditor>
+        <button v-on:click="emptyEditor()">Empty the editor</button>
+
+        <h2>Editor data</h2>
+        <div v-html="editorData" v-highlight></div>
     </div>
 </template>
 
 <script>
-import AppSiteMenu from "@/components/sidebar/site_menu";
-import AppTopBar from "@/components/sidebar/top_bar";
-export default {
-  components: {
-    AppSiteMenu,
-    AppTopBar
-  },
-  data() {
-    return {
-      arNav: [
-        {
-          router_name: "lega-public",
-          content: "Public Network Terms of Service",
-          children: []
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                editor: ClassicEditor,
+                editorData: '<p>Content of the editor.</p>'
+            };
         },
-        {
-          router_name: "lega-private",
-          content: "Private Network Terms of Service",
-          children: []
-        },
-        {
-          router_name: "lega-privacy",
-          content: "Privacy Policy",
-          children: []
-        },
-        {
-          router_name: "due-start",
-          content: "Data Security and Due Process",
-          children: [
-            {
-              router_name: "due-data-security",
-              content: "Data Security Overview",
-              children: []
-            },
-            {
-              router_name: "due-data-subjects",
-              content: "Data Subjects (You)",
-              children: []
-            },
-            {
-              router_name: "due-data-controllers",
-              content: "Data Controllers (Customers)",
-              children: []
-            },
-            {
-              router_name: "due-data-processors",
-              content: "Data Processors (Vendors)",
-              children: []
+        methods: {
+            emptyEditor() {
+                this.editorData = '';
             }
-          ]
-        },
-
-        {
-          router_name: "lega-cookie",
-          content: "Cookie Policy",
-          children: []
-        },
-        {
-          router_name: "lega-contact",
-          content: "Contact",
-          children: []
         }
-      ]
-    };
-  }
-};
+    }
 </script>
-
-<style  lang="scss">
-.wrapper {
-  font-size: 1.15384615rem !important;
-  padding: 10px 5%;
-
-  .gs32 > .grid,
-  .gs32 > .grid--cell {
-    margin: 16px;
-  }
-
-  .fw-bold {
-    font-weight: 700 !important;
-  }
-
-  .lh-sm {
-    line-height: 1.15384615 !important;
-  }
-
-  .mb32 {
-    margin-bottom: 32px !important;
-  }
-
-  a {
-    color: #07c;
-    text-decoration: none;
-  }
-
-  a:hover {
-    color: darkblue;
-  }
-
-  .my4 {
-    margin-top: 4px !important;
-    margin-bottom: 4px !important;
-  }
-
-  .lh-lg {
-    color: #3b4045 !important;
-    line-height: 1.61538462 !important;
-  }
-
-  .fs-body2 {
-    font-size: 1.15384615rem !important;
-  }
-
-  > ul {
-    margin-left: 30px;
-    margin-bottom: 1em;
-  }
-}
-</style>
