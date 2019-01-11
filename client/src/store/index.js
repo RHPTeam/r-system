ce5b6906a98cae8b944d6932a77c2559d1500cd0
lnguyen24794 <lnguyen24794@gmail.com>
2019-01-11 22:24:15 +0700

    Revert "markdown editor"
    This reverts commit ea98808d1ae3b82bedf61b0b88db00aefe328251.

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,10 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import markdown from "./modules/markdown";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
-  modules:{
-    markdown
-  }
+  actions: {}
 });
