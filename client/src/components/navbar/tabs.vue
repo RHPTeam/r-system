ab85205a8daaff83ac5be433e989fd59de9c7d92
lnguyen24794 <lamnt@fasttrack.edu.vn>
2019-01-14 09:59:13 +0700

    Update tabs.vue

--- client/src/components/navbar/tabs.vue
+++ client/src/components/navbar/tabs.vue
@@ -2,8 +2,8 @@
   <div class="content--tabs">
     <v-tabs show-arrows>
 
-      <v-tab class=".nav--link"  v-for="(item,index) in items" :key="index" :href="'#tab-' + item.url">
-         {{ item.name }} <span v-if="item.qty>0" class="badge badge_light">212</span>
+      <v-tab class=".nav--link" v-for="(item,index) in items" :key="index" :href="'#tab-' + item.url">
+        {{ item.name }} <span v-if="item.qty>0" class="badge badge_light">212</span>
       </v-tab>
 
       <!-- <v-tabs-items>
@@ -31,7 +31,7 @@
 <style lang="scss">
   .content--tabs {
     .badge {
-      margin-left:10px;
+      margin-left: 10px;
       position: relative;
       top: -1px;
       color: #776ab0;
@@ -41,8 +41,8 @@
       border: 1px solid #a8a8a8;
     }
 
-    .v-tabs__div >a:hover:not(.v-tabs__item--active) {
-      color:#776ab0 !important;
+    .v-tabs__div>a:hover:not(.v-tabs__item--active) {
+      color: #776ab0 !important;
     }
 
     .v-tabs__item {
