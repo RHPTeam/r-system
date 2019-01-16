e9fb4166328cca637c5594f40b930e1bd720b933
lnguyen24794 <lnguyen24794@gmail.com>
2019-01-16 22:53:34 +0700

    Update homepage.vue

--- client/src/views/homepage/homepage.vue
+++ client/src/views/homepage/homepage.vue
@@ -18,81 +18,48 @@
 </template>
 
 <script>
-import AppInfo from "./info";
-import AppContent from "./content";
-import AppUserJob from "@/components/user/user-jobs";
-import AppSidebar from "@/components/sidebar/sidebar-left";
-import AppTopbar from "@/components/navbar/navbar";
-export default {
-  components: {
-    AppSidebar,
-    AppTopbar,
-    AppInfo,
-    AppContent,
-    AppUserJob
-  }
-};
+  import AppInfo from "./components/info";
+  import AppContent from "./components/content";
+  import AppUserJob from "@/components/user/user-jobs";
+  import AppSidebar from "@/components/sidebar/sidebar-left";
+  import AppTopbar from "@/components/navbar/navbar";
+  import AppFooter from "./components/footer";
+  export default {
+    components: {
+      AppSidebar,
+      AppTopbar,
+      AppInfo,
+      AppContent,
+      AppUserJob,
+      AppFooter
+    },
+    data(){
+      return {
+        items: [{
+            name: "Intersting",
+            url: "intersting"
+          },
+          {
+            name: "Feature",
+            url: "feature",
+            qty: 212
+          },
+          {
+            name: "Hot",
+            url: "Week"
+          },
+          {
+            name: "Month",
+            url: "questioners"
+          },
+
+        ]
+      }
+    }
+  };
+
 </script>
 
 <style lang="scss" scope>
-.main--wrapper {
-  font-family: "Quicksand", sans-serif;
-  position: relative;
-  color: #3b4045;
-
-  .page--wrapper {
-    background-color: #fff;
-    display: block;
-    margin-left: 250px;
-    padding-top: 64px;
-    position: relative;
-    transition: 0.2s ease-in;
-  }
-
-  .page--content {
-    background-color: #fafafa;
-
-    .main {
-      background-color: #fff;
-      width: 100%;
-    }
-  }
-
-  /*RESPONSIVE MENU LEFT-BAR*/
-  // Extra small devices (portrait phones, less than 576px)
-  // No media query for `xs` since this is the default in Bootstrap
-
-  // Small devices (landscape phones, 576px and up)
-  @media (min-width: 576px) {
-  }
-
-  // Medium devices (tablets, 768px and up)
-
-  @media (max-width: 768px) {
-    .page--wrapper {
-      margin-left: 0;
-
-      .page--content {
-        flex-direction: column;
-      }
-    }
-  }
-
-  @media (min-width: 768px) {
-    .page--wrapper {
-      margin-left: 70px;
-    }
-  }
-
-  // Large devices (desktops, 992px and up)
-  @media (min-width: 992px) {
-  }
-
-  // Extra large devices (large desktops, 1200px and up)
-  @media (min-width: 1200px) {
-    .page--wrapper {
-      margin-left: 250px;
-    }
-  }
-}
+ @import "homepage.scss";
 </style>
