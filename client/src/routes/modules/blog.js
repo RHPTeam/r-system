ec6a47d1bc1c6e43de8c4883f455ead9da12ad58
ChinhHo <tuyetchinh1996@mail.com>
2019-01-18 02:56:33 +0700

    delete posts file in blog

--- client/src/routes/modules/blog.js
+++ client/src/routes/modules/blog.js
@@ -1,29 +1,27 @@
 /** When your routing table is too long, you can split it into small modules**/
 const userRouter = {
-  path: "/blog",
-  component: () => import("@/views/blog/index"),
-  children: [
-    {
-      path: "",
-      name: "blog-home",
-      component: () => import("@/views/blog/blog")
-    },
-    {
-      path: "posts",
-      name: "posts",
-      component: () => import("@/views/blog/posts")
-    },
-    {
-      path: "posts/:id",
-      name: "post",
-      component: () => import("@/views/blog/post")
-    },
-    {
-      path: "categories/:id",
-      name: "posts-category",
-      component: () => import("@/views/blog/posts-category")
-    }
-  ]
+    path: "/blog",
+    component: () =>
+        import ("@/views/blog/index"),
+    children: [{
+            path: "",
+            name: "blog-home",
+            component: () =>
+                import ("@/views/blog/blog")
+        },
+        {
+            path: "posts/:id",
+            name: "post",
+            component: () =>
+                import ("@/views/blog/blog-detail")
+        },
+        {
+            path: "categories",
+            name: "posts-category",
+            component: () =>
+                import ("@/views/blog/posts-category")
+        }
+    ]
 };
 
 export default userRouter;
