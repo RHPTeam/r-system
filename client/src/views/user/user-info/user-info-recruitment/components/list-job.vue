5e0ceb530c830b87cd1147ff43d05f000d33664b
lnguyen24794 <lnguyen24794@gmail.com>
2019-01-18 00:32:32 +0700

    add pagination for recruitment

--- client/src/views/user/user-info/user-info-recruitment/components/list-job.vue
+++ client/src/views/user/user-info/user-info-recruitment/components/list-job.vue
@@ -3,18 +3,20 @@
     <div class="title--job r">
       <h2>Danh sách công việc</h2>
     </div>
-    <div v-for="(n,index) in 10" :key="index">
+    <div v-for="(n,index) in 9" :key="index">
        <app-job/>
     </div>
-   
+    <app-paginate/>
   </div>
 </template>
 
 <script>
   import AppJob from "./item-job";
+  import AppPaginate from "./paginate";
   export default {
     components: {
-      AppJob
+      AppJob,
+      AppPaginate
     }
   }
 
