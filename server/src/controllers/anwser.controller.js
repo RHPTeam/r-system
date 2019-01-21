6732da9194be3bd71f7f1bd7c1501fe669e9dd68
trantoan960 <trantoan.fox.97@gmail.com>
2019-01-21 08:17:53 +0700

    bla bla

--- server/src/controllers/anwser.controller.js
+++ server/src/controllers/anwser.controller.js
@@ -1,25 +1,158 @@
 /**
  * create controller Anwser for project
- * author: 
- * date up: 
- * date to: 
+ * author: quangnc
+ * date up: 16/01/2019
+ * date to: 17/01/2019
  * team: BE-RHP
  */
+const omit = require('lodash/omit');
+const split = require('lodash/split');
+const forEach = require('lodash/forEach');
+
 const Anwser = require('../models/anwser.model');
 const JsonResponse = require('../helpers/json-response')
 
+const includes = {
+  user: "user",
+  question: "question",
+  vote: "vote"
+}
+
 module.exports = {
   /**
-   *
+   * create Anwser
    * @param req
    * @param res
    * @param next
    */
-  index: async (req, res, next) => {
+  createAnwser: async (req, res, next) => {
     try {
-      return res.json(JsonResponse("", 200, "success", false));
+      const data_anwser = req.body;
+      if (!data_anwser.content) {
+        return res.json(JsonResponse("", 403, "Content Anwser is require", false))
+      }
+
+      const anwser = await new Anwser(data_anwser);
+      anwser.save((errors, data) => {
+        if (errors) {
+          return res.json(JsonResponse("", 404, errors, false));
+        }
+        return res.json(JsonResponse(data, 200, "create Anwser success", false));
+      })
     } catch (error) {
-      console.log(error)
+      next(err)
+    }
+  },
+
+  /**
+   * Get all Anwser
+   * @param req
+   * @param res
+   * @param next
+   */
+  getAllAnwser: async (req, res, next) => {
+    try {
+      const {
+        query,
+      } = req;
+      const include = []
+      forEach(split(query._includes, ','), i => {
+        if (includes[i]) {
+          include.push(includes[i]);
+        }
+      });
+      return await Anwser.find(omit(query, ['_includes']))
+        .populate(include)
+        .exec((errors, data) => {
+          if (errors) {
+            return res.json(JsonResponse("", 401, errors, false))
+          }
+          return res.json(JsonResponse(data, 200, "", false))
+        });
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  /**
+   * Get one Anwser
+   * @param req
+   * @param res
+   * @param next
+   */
+  getOneAnwser: async (req, res, next) => {
+    try {
+      return res.json(JsonResponse(req.anwser, 200, "", false))
+    } catch (error) {
+      next(next)
+    }
+  },
+
+  /**
+   * update Anwser by id 
+   * @param req
+   * @param res
+   * @param next
+   */
+  updateAnwser: async (req, res, next) => {
+    try {
+      const {
+        anwser,
+        body
+      } = req;
+      if (!body.content) {
+        return res.json(JsonResponse("", 403, "Content Anwser is require", false))
+      }
+      return await anwser.updateOne(body, (errors, data) => {
+        if (errors) {
+          return res.json(JsonResponse("", 404, errors, false))
+        }
+        res.json(JsonResponse("", 200, `update user success`, false))
+      })
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  /**
+   * delete Anwser by id
+   * @param req
+   * @param res
+   * @param next
+   */
+  deleteAnwser: async (req, res, next) => {
+    try {
+      return await Anwser.deleteOne(req.anwser, err => {
+        if (err) {
+          res.json(JsonResponse("", 404, errors, false))
+        }
+        res.send(JsonResponse("", 200, `Delete anwser ${req.anwser.name}`, false))
+      })
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  /**
+   * get Anwser by id
+   * @param req
+   * @param res
+   * @param next
+   * @param id
+   */
+  getByIdAnwser: async (req, res, next, id) => {
+    try {
+      const anwser = await Anwser.findById({
+        _id: id
+      })
+      .populate(`_${req.query._includes}`);;
+      if (!anwser) {
+        return res.json(JsonResponse("", 404, `Anwser doesn't exist`, false));
+      }
+      req.anwser = anwser;
+      next();
+    } catch (error) {
+      next(error)
     }
   },
 }
--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -5,68 +5,36 @@
  * date to: 11/01/2019
  * team: BE-RHP
  */
-const validator = require('validator');
+
 const passport = require('passport');
+const omit = require('lodash/omit');
+const split = require('lodash/split');
+const forEach = require('lodash/forEach');
+
 //const joi = require('joi');
 const User = require('../models/user.model');
+const Notification = require('../models/notification.model');
+const Question = require('../models/question.model');
+const Anwser = require('../models/anwser.model');
+const Permission = require('../models/permission.model');
+const Favoritie = require('../models/favorites.model');
+
 const JsonResponse = require('../helpers/json-response')
+const validateUser = require('../validator/user');
 
-// validate form data 
-validateMemberForm = async payload => {
-  const errors = {};
-  let isFormValid = true;
-  let message = '';
-
-  if (!payload || typeof payload.email !== 'string' || payload.email.trim().length === 0 || !validator.isEmail(payload.email)) {
-    isFormValid = false;
-    errors.email = 'Email is require';
-  }
-
-  if (!payload || typeof payload.password !== 'string' || payload.password.trim().length < 6) {
-    isFormValid = false;
-    errors.password = 'Password is require';
-  }
-
-  if (!payload || typeof payload.name !== 'string' || payload.name.trim().length === 0) {
-    isFormValid = false;
-    errors.name = 'User name is require';
-  }
-
-  if (!payload || typeof payload.nameDisplay !== 'string') {
-    isFormValid = false;
-    errors.nameDisplay = 'Name display is require';
-  }
-
-  if (!isFormValid) {
-    message = 'Xảy ra sự cố thông tin bạn cung cấp.';
-  }
-
-  return {
-    success: isFormValid,
-    message,
-    errors
-  };
-}
-
-const findUserEmailOrName = async data => {
-  try {
-    return await User.find()
-      .or([{
-          email: data.email
-        },
-        {
-          nameDisplay: data.nameDisplay
-        }
-      ])
-      .exec();
-  } catch (error) {
-
-  }
+const includes = {
+  notifications: "notifications",
+  questions: "questions",
+  anwsers: "anwsers",
+  reputation: "reputation",
+  permissions: "permissions",
+  ranks: "ranks",
+  favorities: "favorities"
 }
 
 module.exports = {
   /**
-   * Create one user
+   * Create one user (Register)
    * @param req
    * @param res
    * @param next
@@ -74,12 +42,20 @@ module.exports = {
   createUser: async (req, res, next) => {
     try {
       const list_user = req.body;
-      const validationResult = await validateMemberForm(list_user);
+      const validationResult = await validateUser.createUser(list_user);
       if (!validationResult.success) {
         return res.json(JsonResponse("", 403, validationResult.errors, false))
       }
       //check email or nameDisplay exist
-      if (findUserEmailOrName(list_user).length > 0) {
+      const findUser = await User.find()
+        .or([{
+            email: list_user.email
+          },
+          {
+            nameDisplay: list_user.nameDisplay
+          }
+        ]);
+      if (findUser.length > 0) {
         return res.json(JsonResponse("", 404, "Email or nameDisplay is exist", false))
       }
 
@@ -88,10 +64,10 @@ module.exports = {
         if (err) {
           return res.json(JsonResponse("", 404, "Email or nameDisplay is exist.", false));
         }
-        return res.json(JsonResponse("", 200, "create user success", false))
+        return res.json(JsonResponse("", 200, "created user successfully!", false))
       })
     } catch (error) {
-      console.log(error)
+      next(error)
     }
   },
 
@@ -103,8 +79,19 @@ module.exports = {
    */
   getAllUsers: async (req, res, next) => {
     try {
-      return await User.find()
+      const {
+        query,
+      } = req;
+      const include = []
+      forEach(split(query._includes, ','), i => {
+        if (includes[i]) {
+          include.push(includes[i]);
+        }
+      });
+
+      return await User.find(omit(query, ['_includes']))
         .select('_id userid name nameDisplay email avatar title about ')
+        .populate(include)
         .exec((errors, data) => {
           if (errors) {
             return res.json(JsonResponse("", 401, errors, false))
@@ -112,7 +99,7 @@ module.exports = {
           return res.json(JsonResponse(data, 200, "", false))
         });
     } catch (error) {
-      console.log(error);
+      next(error);
     }
   },
 
@@ -120,12 +107,13 @@ module.exports = {
    * get one user
    * @param req
    * @param res
+   * @param next
    */
-  getOneUser: async (req, res) => {
+  getOneUser: async (req, res, next) => {
     try {
-      return res.json(req.user);
+      return res.json(JsonResponse(req.user, 200, "", false));
     } catch (error) {
-      console.log(error)
+      next(error);
     }
   },
 
@@ -133,26 +121,34 @@ module.exports = {
    * update user by id
    * @param req
    * @param res
+   * @param next
    */
-  updateUser: async (req, res) => {
+  updateUser: async (req, res, next) => {
     try {
       const {
         user,
         body
       } = req;
-      if (findUserEmailOrName(body).length > 1) {
+
+      const findUser = await User.find()
+        .or([{
+            email: body.email
+          },
+          {
+            nameDisplay: body.nameDisplay
+          }
+        ]);
+      if (findUser.length > 1) {
         return res.json(JsonResponse("", 404, "Email or nameDisplay is exist", false))
       }
-      return await User.findByIdAndUpdate({
-        _id: user._id
-      }, body, (errors, data) => {
+      return await user.updateOne(body, (errors, data) => {
         if (errors) {
           return res.json(JsonResponse("", 404, errors, false))
         }
-        res.json(JsonResponse(data, 200, "", false))
+        res.json(JsonResponse("", 200, `update user success`, false))
       })
     } catch (error) {
-      console.log(error);
+      next(error);
     }
   },
 
@@ -160,22 +156,18 @@ module.exports = {
    * delete user by id 
    * @param req
    * @param res
+   * @param next
    */
-  deleteUser: async (req, res) => {
+  deleteUser: async (req, res, next) => {
     try {
-      const {
-        user
-      } = req;
-      return await User.findByIdAndRemove({
-        _id: user._id
-      }, (errors, data) => {
-        if (errors) {
+      return await req.user.remove(err => {
+        if (err) {
           res.json(JsonResponse("", 404, errors, false))
         }
-        res.send(JsonResponse("", 200, `Delete user ${user.name} success`, false))
+        res.send(JsonResponse("", 200, `Delete user ${req.user.name} success`, false))
       })
     } catch (error) {
-      console.log(error);
+      next(error);
     }
   },
 
@@ -191,7 +183,8 @@ module.exports = {
       const user = await User.findById({
           _id: id
         })
-        .select('_id userid name nameDisplay email avatar title about ')
+        .select('_id userid name nameDisplay email avatar title about _permissions')
+        .populate(`_${req.query._includes}`)
         .exec();
       if (!user) {
         return res.json(JsonResponse("", 404, `User not found`, false));
@@ -199,7 +192,7 @@ module.exports = {
       req.user = user;
       next();
     } catch (error) {
-      console.log(error)
+      next(error);
     }
   },
 
@@ -227,7 +220,7 @@ module.exports = {
    * @param res
    */
   isLogin: (req, res) => {
-    req.end();
+    res.end();
   },
 
   /**
@@ -236,7 +229,160 @@ module.exports = {
    * @param res
    */
   logoutUser: (req, res) => {
-    req.logout();
-  }
+    res.logout();
+    res.end();
+  },
+
+  createUserPopulate: async (req, res, next) => {
+    try {
+      const {
+        query
+      } = req;
+      const user = await User.findById(req.user._id);
+      switch (query._includes) {
+        case "notifications":
+          const notification = await new Notification(req.body);
+          notification._users = user;
+          await notification.save();
+          user._notifications.push(notification._id);
+          break;
+        case "questions":
+          const question = await new Question(req.body);
+          question._user = user;
+          question.save();
+          user._questions.push(question._id);
+          break;
+        case "anwsers":
+          const anwser = await new Anwser(req.body);
+          anwser._user = question;
+          anwser.save();
+          user._anwsers.push(anwser._id);
+          break;
+        case "permissions":
+          const permission = await new Permission(req.body);
+          permission._user = question;
+          permission.save();
+          user._permissions.push(permission._id);
+          break;
+        case "favorities":
+          const favoritie = await new Favoritie(req.body);
+          favoritie._user = question;
+          favoritie.save();
+          user._anwsers.push(favoritie._id);
+          break;
+        default:
+          return res.json(JsonResponse("", 404, `error query`, false))
+      }
+      return await user.save((err, data) => {
+        if (err) {
+          return next(JsonResponse("", 404, `error`, false))
+        }
+        res.json(JsonResponse("", 200, "Create success", false))
+      })
+    } catch (error) {
+      next(error)
+    }
+  },
+  /**
+   * localhost:8888/api/v1/questions/5c40cfa476ecf70d00fb1842/populate?_includes=tags&_id=5c40da73c30d5d0f3fecd6d9
+   */
+  deleteUserPopulate: async (req, res, next) => {
+    try {
+      const {
+        query,
+        user
+      } = req;
+      // kiem tra query co _includes and _id ?
+      // ....
+
+      switch (query._includes) {
+        case "notifications":
+          const checkNotification = await user._notifications && user._notifications.map(i => i._id.toString() === query._id.toString())
+          if (!checkNotification) {
+            return next(JsonResponse("", 404, `Notification not found in User`, false))
+          }
+          await user._notifications.remove(query._id);
+          break;
+        case "questions":
+          const checkQuestion = await user._questions && user._questions.map(i => i._id.toString() === query._id.toString())
+          if (!checkQuestion) {
+            return next(JsonResponse("", 404, `Anwser not found in User`, false))
+          }
+          await user._questions.remove(query._id);
+          break;
+        case "anwsers":
+          const checkAnwser = await user._anwsers && user._anwsers.map(i => i._id.toString() === query._id.toString())
+          if (!checkAnwser) {
+            return next(JsonResponse("", 404, `Anwser not found in Question`, false))
+          }
+          await user._anwsers.remove(query._id);
+          break;
+        case "permissions":
+          const checkPermission = await user._permissions && user._permissions.map(i => i._id.toString() === query._id.toString())
+          if (!checkPermission) {
+            return next(JsonResponse("", 404, `Permission not found in Question`, false))
+          }
+          await user._permissions.remove(query._id);
+          break;
+        case "favorities":
+          const checkFavorite = await user._favorities && user._favorities.map(i => i._id.toString() === query._id.toString())
+          if (!checkFavorite) {
+            return next(JsonResponse("", 404, `Anwser not found in Question`, false))
+          }
+          await user._favorities.remove(query._id);
+          break;
+        default:
+          return
+      }
+      await user.save((err, data) => {
+        if (err) {
+          return next(JsonResponse("", 404, `error`, false))
+        }
+        res.json(JsonResponse("", 200, "Delete success", false))
+      })
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  // createPermissionUser: async (req, res) => {
+  //   try {
+  //     const user = req.user;
+  //     const newPermission = await new Permission(req.body);
+  //     newPermission._user = user;
+  //     await newPermission.save();
+  //     user.permission(newPermission.id)
+  //     res.json(JsonResponse("", 200, `Create permission by user`, false))
+  //   } catch (error) {
+  //     next(error);
+  //   }
+  // },
+
+  // getPermissionUser: async (req, res) => {
+  //   try {
+  //     const user = req.user;
+  //     // const permissionUser = user.populate('_permissions')
+  //     const permissionUser = await User.findById(user._id).populate(' _permissions');
+  //     res.json(JsonResponse(permissionUser, 200, "", false))
+  //   } catch (error) {
+  //     next(error);
+  //   }
+  // },
+
+  // deletePermissionInUser: async (req, res, next) => {
+  //   try {
+  //     const {
+  //       permissionId
+  //     } = req.params;
+  //     const check = req.user.isPermission(permissionId)
+  //     if (!check) {
+  //       return next(JsonResponse("", 404, `User not found role`, false))
+  //     }
+  //     await req.user.unPermission(permissionId);
+  //     res.json(JsonResponse("", 200, "Delete success", false))
+  //   } catch (error) {
+  //     next(error)
+  //   }
+  // }
 
 }
