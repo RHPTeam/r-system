710616e2232071ed9cd730564757297cc2fc6127
trantoan960 <trantoan.fox.97@gmail.com>
2019-01-22 05:24:07 +0700

    Update file  before merge Khang branch version 22012019

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -47,6 +47,64 @@ module.exports = {
     who._postsList.push(post);
     await who.save();
 
-    res.json(JsonResponse("", 200, "Thêm dữ liệu thành công", false))
+    res.json(JsonResponse("", 200, "Thêm dữ liệu thành công! <3", false))
   },
+
+  /**
+   * @Name: Create Post by User
+   * @param req
+   * @param res
+   * @param next
+   */
+  createTag: async (req, res, next) => {
+    const who = await User.findById(req.value.body._owner);
+    const newPost = req.value.body;
+    delete newPost._owner;
+
+    const post = new Post(newPost);
+    post._owner = who;
+    await post.save();
+
+    who._postsList.push(post);
+    await who.save();
+
+    res.json(JsonResponse("", 200, "Thêm dữ liệu thành công! <3", false))
+  },
+
+  /**
+   * @Name: Update Post by User
+   * @param req
+   * @param res
+   * @param next
+   */
+  update: async (req, res, next) => {
+    const { postId } = req.value.params;
+    const newPost = req.value.body;
+
+    const data = await Post.findByIdAndUpdate(postId, newPost);
+
+    res.json(JsonResponse(data, 201, "Cập nhật thông tin thành công! :D"))
+  },
+
+  /**
+   * @Name: Delete Post by User
+   * @param req
+   * @param res
+   * @param next
+   */
+
+  delete: async (req, res, next) => {
+    const { postId } = req.value.params;
+
+    const post = await Post.findById(postId);
+    if (!post) return res.json(JsonResponse("", 403, "Dữ liêu không tồn tại, thử lại! :)", true))
+
+    const who = await User.findById(post._owner);
+
+    await post.remove();
+    who._postsList.pull(post);
+    await who.save();
+
+    res.json(JsonResponse("", 200, "Xóa dữ liệu thành công! T_T", false));
+  }
 }
